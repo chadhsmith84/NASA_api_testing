e2aeb0b4eced4d842f2226fded717c3961afbdd0
chadhsmith84 <chs.smith@gmail.com>
2024-05-06 17:01:10 -0400

    added startdate and enddate and parsing for list vs dict return data

--- NASA_api_testing/APIs.py
+++ NASA_api_testing/APIs.py
@@ -1,5 +1,5 @@
-import os, requests, urllib3, json, datetime
-from unittest import defaultTestLoader
+import os, requests, urllib, json, datetime
+import webbrowser
 # import pprint
 import config
 from log import Logger
@@ -14,20 +14,41 @@ class NASA_APIs():
         raise ValueError('no valid date format found')
 
     def openImage(self, img):
-        os.startfile(img)        
-        
+        self.log.info('opening image: {}'.format(img))
+        os.startfile(img)
+
+    def openUrl(self, url):
+        '''
+        If new is 0, the url is opened in the same browser window if possible. 
+        If new is 1, a new browser window is opened if possible. 
+        If new is 2, a new browser page ("tab") is opened if possible
+        '''
+        self.log.info('opening url: {}'.format(url))
+
+        # checking url is valid
+        response = urllib.request.urlopen(url)
+
+        # opening url if it is valid
+        if response.status == 200:
+            webbrowser.open(url, new = 0)
+        else:
+            raise ValueError('received response: {}, reason: {} for url: {}'.format(response.status, response.reason, response.url))
+
     def downloadPicUrl(self, dir, url, date):
         self.log.info('attempting to download image from url: {}'.format(url))
         response = requests.get(url)
 
         # check response for url call
         if response.status_code == 200:
+
             # creating directory if it doesn't exist
             if not os.path.exists(dir):
                 os.makedirs(dir)
                 
+            # defining download path
+            downloadPath = os.path.join(dir, 'APOD_{}_{}'.format(date, url.split("/")[-1]))
+
             # writing out image
-            downloadPath = os.path.join(dir, 'APOD_{}_{}'.format(date, url.split("/")[-1]))                
             with open(downloadPath, 'wb') as outputImage:
                 outputImage.write(response.content)
 
@@ -38,48 +59,77 @@ class NASA_APIs():
         else:
             self.log.info('failed to download image: {}. Status code: {}'.format(url, response.status_code))
     
-    def fetchAPOD(self, date, saveImage, dir):
-        '''
-        Parameter       Type        Default     Description
-        date 	        YYYY-MM-DD 	today 	    The date of the APOD image to retrieve
-        start_date 	    YYYY-MM-DD 	none 	    The start of a date range, when requesting date for a range of dates. Cannot be used with date.
-        end_date 	    YYYY-MM-DD 	today 	    The end of the date range, when used with start_date.
-        count 	        int 	    none 	    If this is specified then count randomly chosen images will be returned. Cannot be used with date or start_date and end_date.
-        thumbs 	        bool 	    False 	    Return the URL of video thumbnail. If an APOD is not a video, this parameter is ignored.
-        api_key 	    string 	    DEMO_KEY 	api.nasa.gov key for expanded usage
-        '''
+    #def fetchAPOD(self, date, saveImage, dir):
+    def fetchAPOD(self, **kwargs):
+        try:
+            '''
+            Parameter       Type        Default     Description
+            date 	        YYYY-MM-DD 	today 	    The date of the APOD image to retrieve
+            start_date 	    YYYY-MM-DD 	none 	    The start of a date range, when requesting date for a range of dates. Cannot be used with date.
+            end_date 	    YYYY-MM-DD 	today 	    The end of the date range, when used with start_date.
+            count 	        int 	    none 	    If this is specified then count randomly chosen images will be returned. Cannot be used with date or start_date and end_date.
+            thumbs 	        bool 	    False 	    Return the URL of video thumbnail. If an APOD is not a video, this parameter is ignored.
+            api_key 	    string 	    DEMO_KEY 	api.nasa.gov key for expanded usage
+            '''
 
-        self.log.info('Running APOD (Astronomy Pricture of the Day) for date: {}'.format(date))
+            self.log.info('Running APOD (Astronomy Pricture of the Day) for date: {}'.format(kwargs['date']))
         
-        URL_APOD = "https://api.nasa.gov/planetary/apod"
-        params = {
-                'api_key':os.environ['API_SECRET_KEY'],
-                'date':date,
-                }
-        try:         
-            response = requests.get(URL_APOD,params=params)
-            response.raise_for_status()
+            URL_APOD = "https://api.nasa.gov/planetary/apod"
+
+            if kwargs['startDate'] != None and kwargs['endDate'] != None:
+                params = {
+                        'api_key':os.environ['API_SECRET_KEY'],
+                        'start_date':kwargs['startDate'],
+                        'end_date':kwargs['endDate']
+                        }
+            else:
+                params = {
+                        'api_key':os.environ['API_SECRET_KEY'],
+                        'date':kwargs['date']
+                        }
+            try:         
+                response = requests.get(URL_APOD,params=params)
+                # pprint.PrettyPrinter().pprint(response)
+                response.raise_for_status()
             
-        except requests.RequestException as e:
-            self.log.critical(response.text)
+            except requests.RequestException as e:
+                self.log.critical('params passed: {}'.format(params))
+                self.log.critical(response.text)
+                raise ValueError(e)
+
+            # parse json to dictionary
+            responseDict = json.loads(response.text)
+
+            # logging returned data
+            self.log.info('Information about the image')
+
+            # if date range is passed a list is returned from the api
+            if type(responseDict) == list:
+                for items in responseDict:
+                    for key, value in items.items():
+                        self.log.info('{}: {}'.format(key, value.strip()))
+                    if items['media_type'].lower() == 'image' and kwargs['saveImage']:
+                        downloadedImage = self.downloadPicUrl(kwargs['dir'], value['hdurl'], kwargs['date'])
+
+                        # open file with default application
+                        self.openImage(downloadedImage)
+                    else:
+                        self.log.info('Media Type: {} and saveImage value: {} (videos will not be download)'.format(items['media_type'], kwargs['saveImage']))
+                        self.openUrl(items['url'])
+            else:
+                for key, value in responseDict.items():
+                    self.log.info('{}: {}'.format(key, value.strip()))
+
+                    if responseDict['media_type'].lower() == 'image' and kwargs['saveImage']:
+                        downloadedImage = self.downloadPicUrl(kwargs['dir'], responseDict['hdurl'], kwargs['date'])
+
+                        # open file with default application
+                        self.openImage(downloadedImage)
+                    else:
+                        self.log.info('Media Type: {} and saveImage value: {} (videos will not be download)'.format(responseDict['media_type'], kwargs['saveImage']))
+                        self.openUrl(responseDict['url'])
+        except BaseException as e:
             raise ValueError(e)
-
-        # parse json to dictionary
-        responseDict = json.loads(response.text)
-
-
-        # logging returned data
-        self.log.info('Information about the image')
-        for key, value in responseDict.items():        
-            self.log.info('{}: {}'.format(key, value.strip()))
-
-        # pprint.PrettyPrinter().pprint(response)
-
-        if saveImage:
-            downloadedImage = self.downloadPicUrl(dir, responseDict['hdurl'], date)
-
-        # open file with default application
-        self.openImage(downloadedImage)            
         
     def __init__(self):        
         self.todayYYYYMMDD = datetime.datetime.now().strftime("%Y-%m-%d")
--- NASA_api_testing/NASA_api_testing.py
+++ NASA_api_testing/NASA_api_testing.py
@@ -3,18 +3,35 @@ import traceback, os, getopt, sys
 from APIs import NASA_APIs
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], '', ['date='])
+    opts, args = getopt.getopt(sys.argv[1:], '', ['project=', 'date=', 'startDate=', 'endDate='])
     optsDict = {}
     optsDict.update(dict(opts))
 
-    apiCall = NASA_APIs() 
+    apiCall = NASA_APIs()
 
-    if '--date' in optsDict:
-        _tempDate = optsDict['--date']
-        date = apiCall.try_parsing_date(_tempDate)
-    else:
-        date = apiCall.todayYYYYMMDD 
-        
+    if '--project' in optsDict:
+        _project = optsDict['--project']
+        if _project.lower() == 'apod':
+            if '--date' in optsDict and '--startDate' in optsDict and '--endDate' in optsDict:
+                raise ValueError('must provide --date OR --startDate and --endDate. Both cannot be provided')
+            else:
+                if '--date' in optsDict:
+                    _tempDate = optsDict['--date']
+                    date = apiCall.try_parsing_date(_tempDate)
+                else:
+                    date = apiCall.todayYYYYMMDD 
+                if '--startDate' in optsDict:
+                    _tempStartDate = optsDict['--startDate']
+                    startDate = apiCall.try_parsing_date(_tempStartDate)
+                else:
+                    startDate = None
+                if '--endDate' in optsDict:
+                    _tempEndDate = optsDict['--endDate']
+                    endDate = apiCall.try_parsing_date(_tempEndDate)
+                else:
+                    endDate = None
+
+
     '''
     APOD
     
@@ -25,14 +42,14 @@ try:
 
     The full documentation for this API can be found in the APOD API Github repository.
     ''' 
-                     
-    apiCall.fetchAPOD(date, saveImage=True, dir=os.path.join(apiCall.codePath, 'APOD_Images'))
-
-    apiCall.log.info('Process Complete')
-
-    apiCall.log.closeLogger()
-except:
-    apiCall.log.critical(traceback.print_exc())
-    apiCall.log.closeLogger()    
+    try:
+        if _project.lower() == 'apod':
+            apiCall.fetchAPOD(date=date, startDate=startDate, endDate=endDate, saveImage=False, dir=os.path.join(apiCall.codePath, 'APOD_Images'))
     
-    
+
+        apiCall.log.info('Process Complete')
+    except BaseException as e:
+        apiCall.log.critical(e)
+        apiCall.log.closeLogger()
+except BaseException as e:
+    traceback.print_exc()
